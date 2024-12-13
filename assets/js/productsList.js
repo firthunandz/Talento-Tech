740dbbb25158d63e646a68398af9e5ef6789233d
firthunandz <fernandonchiesa@gmail.com>
2024-12-13 14:05:44 -0300

    Pagina productos dinamica

---
+++ assets/js/productsList.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chucrut",
+    description: "Col fermentada tradicional, rica en probióticos y sabor ácido.",
+    ingredients: "Col blanca, sal",
+    price: 5000,
+    img: './assets/images/chucrutblanco.jpg'
+  },
+  {
+    id: 2,
+    name: "Chucrut Morado",
+    description: "Variedad de chucrut hecha con col morada, con un sabor único y vibrante.",
+    ingredients: "Col morada, sal",
+    price: 5000,
+    img: './assets/images/chucrutmorado.jpg'
+  },
+  {
+    id: 3,
+    name: "Kimchi",
+    description: "Fermento coreano picante y lleno de sabor, ideal para acompañar comidas.",
+    ingredients: "Col akusay, ajo, jengibre, chile, cebolla, nabo",
+    price: 7000,
+    img: './assets/images/kimchifrasco.jpg'
+  },
+  {
+    id: 4,
+    name: "Sriracha",
+    description: "Salsa picante fermentada con un equilibrio perfecto entre dulce, ácido y picante.",
+    ingredients: "Chiles rojos, ajo, vinagre, azúcar, sal",
+    price: 5000,
+    img: './assets/images/picante.jpg'
+  },
+  {
+    id: 5,
+    name: "Vinagre de Manzana",
+    description: "Vinagre natural y artesanal, ideal para ensaladas y aderezos.",
+    ingredients: "Manzanas, Agua, azúcar",
+    price: 4500,
+    img: './assets/images/vinagrebotella.jpg'
+  },
+  {
+    id: 6,
+    name: "Pepinos Pickles",
+    description: "Pepinos encurtidos en una mezcla de vinagre y especias, perfectos como snack o acompañamiento.",
+    ingredients: "Pepinos, vinagre, azúcar, eneldo, especias",
+    price: 4000,
+    img: './assets/images/pickles.jpg'
+  }
+];
